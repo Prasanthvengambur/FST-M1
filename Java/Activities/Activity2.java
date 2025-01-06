d33eeba5f40e129313e8a425978fee331022ac30
Prasanthvengambur <prasanth.vengambur.sowndararayar@ibm.com>
2025-01-06 19:08:19 +0530

    Add files via upload

---
+++ Java/Activities/Activity2.java
@@ -0,0 +1,42 @@
+package activities;
+
+public class Activity2 {
+
+	public static void main(String[] args) {
+		// Create the object for the class
+		Activity2 actObj = new Activity2();
+		//  Create the integer array
+		int[] nums = { 10, 77, 10, 54, -11, 10};
+		
+		// Set the number to search for
+		int searchNum = 10;
+		int fixedSum = 30;
+		
+		// Call the result function and print the result
+		System.out.println("Result: " + actObj.result(nums, searchNum, fixedSum));
+	}
+	
+	public boolean result(int[] nums, int searchNum, int fixedSum) {
+		// Temp variable to hold the calculated sum
+		int tempSum = 0;
+		
+		//Loop through the array to calculate sum
+		for(int number : nums) {
+		//  Check if the value is searchNum
+			if(number == searchNum) {
+				// Add the value
+				tempSum += searchNum;
+	}
+		
+		//Check if tempSum is greater than fixed Sum
+		if(tempSum > fixedSum) {
+			// Condition fails, break loop
+			break;
+		}
+		
+	}
+       
+	  return fixedSum == tempSum;
+	  
+    }
+}
